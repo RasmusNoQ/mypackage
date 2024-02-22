7bb4f4cad1df27cc24c1aafa821e1b989f576500
Daniel Lucas <danyyel770@gmail.com>
2024-02-22 13:17:11 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mypackage
+This was created as an example of how to publish your own Python package.
+
+# How to install
+1. 
---
+++ mypackage/myModule.py
@@ -0,0 +1,27 @@
+def top_n(items,n):
+    """
+        Return the top n items in a list in descending order
+
+        Args:
+            items(array): list or array-like object
+            n (int): num of items to return
+        
+        Return:
+            array: top n items in descending order
+        Egs:
+            >> top_n([8,3,2,7,4],3)
+            << 8,7,4 
+    """
+    
+    for i in range(n):
+        for j in range(len(items)-1-i):
+            
+            if items[j] > items[j+1]:
+                items[j], items[j+1] = items[j+1],items[j]
+                
+                
+    # get last two items
+    top_n = items[-n:]
+    
+    #return in descending order
+    return top_n[::-1]
